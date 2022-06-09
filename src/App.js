907867b2a163ce3cee61eb0608f824e5fdc6d245
xXnielsXx <nielsmichael@comcast.net>
2022-06-09 10:13:30 +0900

    Add Meals component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <>
       <Header />
+      <Meals />
     </>
   );
 }
---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,42 @@
+import classes from "./AvailableMeals.module.css";
+
+const DUMMY_MEALS = [
+  // could also import from database but for now use object array
+  {
+    id: "m1",
+    name: "Sushi",
+    description: "Finest fish and veggies",
+    price: 22.99,
+  },
+  {
+    id: "m2",
+    name: "Schnitzel",
+    description: "A german specialty!",
+    price: 16.5,
+  },
+  {
+    id: "m3",
+    name: "Barbecue Burger",
+    description: "American, raw, meaty",
+    price: 12.99,
+  },
+  {
+    id: "m4",
+    name: "Green Bowl",
+    description: "Healthy...and green...",
+    price: 18.99,
+  },
+];
+
+const AvailableMeals = () => {
+  // Can simply map our DUMMY_MEALS in the return statement, but storing it in a helper var like this produces leaner JSX code
+  const mealsList = DUMMY_MEALS.map((meal) => <li></li>);
+
+  return (
+    <section className={classes.meals}>
+      <ul>{mealsList}</ul>
+    </section>
+  );
+};
+
+export default AvailableMeals;
