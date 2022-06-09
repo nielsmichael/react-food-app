000f3a6b832691d6359a66c2ff4a2229b65f4053
xXnielsXx <nielsmichael@comcast.net>
2022-06-09 19:45:38 +0900

    Add MealItem component and implement

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,4 +1,5 @@
 import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
 
 const DUMMY_MEALS = [
@@ -31,7 +32,14 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = () => {
   // Can simply map our DUMMY_MEALS in the return statement, but storing it in a helper var like this produces leaner JSX code
-  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
 
   return (
     <section className={classes.meals}>
