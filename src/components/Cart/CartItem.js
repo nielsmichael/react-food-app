85c5c0c29326cea672bc0df6cde56cdfd9abbb32
xXnielsXx <nielsmichael@comcast.net>
2022-06-26 17:47:58 +0900

    Add cartitem component w/ styling

---
+++ src/components/Cart/CartItem.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import classes from "./Cart.module.css";
+
+const CartItem = (props) => {
+  const price = `$${props.price.toFixed(2)}`;
+
+  return (
+    <li className={classes["cart-item"]}>
+      <div>
+        <h2>{props.name}</h2>
+        <div className={classes.summary}>
+          <span className={classes.price}>{price}</span>
+          <span className={classes.amount}>{props.amount}</span>
+        </div>
+      </div>
+      <div className={classes.action}>
+        <button onClick={props.onRemove}>-</button>
+        <button onClick={props.onAdd}>+</button>
+      </div>
+    </li>
+  );
+};
+
+export default CartItem;
