d149f460b089986b346f77a92460f58562f053f6
xXnielsXx <nielsmichael@comcast.net>
2022-06-27 08:49:27 +0900

    Add visual cart bump when items are added/removed from cart

--- src/components/Layout/HeaderCartBtn.js
+++ src/components/Layout/HeaderCartBtn.js
@@ -1,23 +1,43 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 import classes from "./HeaderCartBtn.module.css";
 
 const HeaderCartBtn = (props) => {
+  const [useBump, setUseBump] = useState(false);
   const cartCtx = useContext(CartContext);
+  const items = cartCtx;
+
   const numOfCartItems = cartCtx.items.reduce((currentNum, item) => {
     return currentNum + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${useBump ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setUseBump(true);
+
+    const timer = setTimeout(() => {
+      setUseBump(false);
+    }, 300);
+
+    // called automatically by react as a cleanup function
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]); // pass items as dependancy
 
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>{numOfCartItems/* // hardcoded for now */}</span>
+      <span className={classes.badge}>{numOfCartItems}</span>
     </button>
   );
 };
